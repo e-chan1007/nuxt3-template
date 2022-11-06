ffe02562212ef1cc974c17e984efc497f84de332
いーちゃん(e_chan1007) <echan041007@gmail.com>
2022-11-06 20:46:21 +0900

    typescript: strict is now default

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,2 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({
-  srcDir: "src",
-  typescript: { strict: true }
-});
+export default defineNuxtConfig({ srcDir: "src" });
